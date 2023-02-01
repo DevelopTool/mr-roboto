bf53ab41fb4949f620935841f38f86c95dc45cde
GrimmTVT <65668096+DevelopTool@users.noreply.github.com>
2023-02-01 14:55:29 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,96 @@
-input.onButtonPressed(Button.B, function () {
-    basic.showString("AAAAAAAAAAAAAAAAAAAAAAAAAa")
+radio.onReceivedNumber(function (receivedNumber) {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        # . # # #
+        # . # . #
+        # . # # #
+        . . . . .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . # .
+        . . . # .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # # .
+        . . . # .
+        . . . # .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . # # . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . # # # .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 255, 2000, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 1, 1, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showIcon(IconNames.SmallDiamond)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 1, 1, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showIcon(IconNames.Diamond)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 1, 1, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showIcon(IconNames.SmallDiamond)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 1, 1, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.Happy)
 })
-basic.showIcon(IconNames.Happy)
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
 })
+basic.showIcon(IconNames.Happy)
